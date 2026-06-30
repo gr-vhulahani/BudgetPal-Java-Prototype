5ac2803344983ae59512a28f144064fb4d0bccb4
ronewav <Ronewavhulahani123@gmail.com>
2026-06-30 21:56:18 +0200

    Add initial variable declarations for budget management

--- BudgetPal.java
+++ BudgetPal.java
@@ -1,5 +1,30 @@
 public class BudgetPal{
     public static void main(String[] args) {
-        System.out.println("The beginning");
+        
+        //Income variables
+        double incomeAmount;
+        String incomeCategory;
+        String incomeDescription; 
+
+        //Expense variables
+        double expenseAmount;
+        String expenseCategory;
+        String expenseDescription;
+
+        //Budget Calculation Variables
+        double totalIncome; 
+        double totalExpenses; 
+        double surplus;
+        
+        //Savings Goal Variables 
+        double savingsTarget; 
+        double currentSavings; 
+        int monthsRemaining; 
+        double monthlySavingRequired; 
+        
+        //Debt Planner Variables
+        double debtAmount; 
+
+
     }
 }
